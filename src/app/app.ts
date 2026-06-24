b6cfd04d6b492600b22f516671b7e67d5f8edd77
Prisha <prisha@Prishas-MacBook-Air.local>
2026-06-24 09:28:39 +0530

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './login-page/login-page';
+
+
+@Component({
+  selector: 'app-root', 
+  imports: [RouterOutlet,LoginPage],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Myeicher');
+}
