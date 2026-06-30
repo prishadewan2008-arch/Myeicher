d2db57a2975ccfdcdecf99cd5166f620e2b771c7
Prisha <prisha@Prishas-MacBook-Air.local>
2026-06-30 17:45:07 +0530

    Updates

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,16 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
   selectedRole = 'Customer';
 
-  onRoleChange(value: string): void {
-    console.log('Selected dropdown value:', value);
+  onChange(event: string): void {
+    this.selectedRole = event;
+    console.log(event);
   }
+
+  images = [
+    'assets/bg1.svg',
+    'assets/bg2.svg'
+  ]
+
+  currentIndex = 0;
+
+  intervalId: any;
+
+  ngOnInit() {
+    this.intervalId = setInterval(()=>{
+      this.next();
+    }, 3000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  next() { 
+    debugger;
+    this.currentIndex =
+      (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    debugger;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) %
+      this.images.length;
+  }
+
+
+  // next() {
+  //   this.currentIndex++;
+  //   if(this.currentIndex >= this.images.length){
+  //     this.currentIndex = 0;
+  //   }
+  // }
+
+  // prev(){
+  //   this.currentIndex--;
+  //   if(this.currentIndex < 0){
+  //     this.currentIndex = this.images.length - 1;
+  //   }
+  // }
+
 }
