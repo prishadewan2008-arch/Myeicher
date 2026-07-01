19b5223016ca6ebfdd92ea048dda3831e93e3df3
Ashutosh4999 <ashutosh3@vecvcs.in>
2026-07-01 12:44:11 +0530

    login page updated

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,69 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  afterNextRender,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnDestroy,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-// import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-
 
 @Component({
   selector: 'app-login-page',
   imports: [FormsModule, CommonModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
-  // providers: [NgbCarouselConfig]
 })
-export class LoginPage {
+export class LoginPage implements OnDestroy {
+  private readonly cdr = inject(ChangeDetectorRef);
+
   selectedRole = 'Customer';
+  currentIndex = 0;
+  prevIndex = 0;
+
+  images = [
+    'assets/bg1.svg',
+    'assets/bg2.svg',
+    'assets/bg3.svg',
+    'assets/bg4.svg',
+    'assets/bg5.svg',
+  ];
+
+  private intervalId?: ReturnType<typeof setInterval>;
+
+  constructor() {
+    afterNextRender(() => {
+      this.intervalId = setInterval(() => {
+        this.nextSlide();
+        this.cdr.detectChanges();
+      }, 3000);
+    });
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }  nextSlide(): void {
+    this.prevIndex = this.currentIndex;
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  getSlideClass(index: number): string {
+    if (index === this.currentIndex) {
+      return 'active';
+    }
+    if (index === this.prevIndex && this.prevIndex !== this.currentIndex) {
+      return 'prev';
+    }
+    return 'hidden';
+  }
 
   onChange(event: string): void {
     this.selectedRole = event;
     console.log(event);
   }
-
-  images = [
-    'assets/bg1.svg',
-    'assets/bg2.svg'
-  ]
-
-  currentIndex = 0;
-
-  intervalId: any;
-
-  ngOnInit() {
-    this.intervalId = setInterval(()=>{
-      this.next();
-    }, 3000);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
-  }
-
-  next() { 
-    debugger;
-    this.currentIndex =
-      (this.currentIndex + 1) % this.images.length;
-  }
-
-  prev() {
-    debugger;
-    this.currentIndex =
-      (this.currentIndex - 1 + this.images.length) %
-      this.images.length;
-  }
-
-
-  // next() {
-  //   this.currentIndex++;
-  //   if(this.currentIndex >= this.images.length){
-  //     this.currentIndex = 0;
-  //   }
-  // }
-
-  // prev(){
-  //   this.currentIndex--;
-  //   if(this.currentIndex < 0){
-  //     this.currentIndex = this.images.length - 1;
-  //   }
-  // }
-
 }
